223a2db9aec7526b14eaad4b0be9f1d66c107be6
tylteng <teng.ty4@gmail.com>
2017-11-09 09:38:46 -0500

    revised exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,8 +1,8 @@
 emotions = {
-"joy" => 1..3
-"love" => 1..3
-"sadness" => 1..3
-"fear" => 1..3
-"courage" => 1..3
+"joy" => 1..3,
+"love" => 1..3,
+"sadness" => 1..3,
+"fear" => 1..3,
+"courage" => 1..3,
 "anger" => 1..3
 }
